93b9b863e55e683dae7a657a88def22542cf938c
Itallo-Diniz <jose.diniz@ccc.ufcg.edu.br>
2022-03-02 00:54:25 -0300

    Adcionado componente incial de cabecalho

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import Login from '../pages/Login';
 import Cadastro from '../pages/Cadastro';
+import Cabecalho from '../components/Cabecalho';
 
 function HealthTrackRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Login />} exact />
       <Route path="/sign-up" element={<Cadastro />} />
+      <Route path="/home" element={<Cabecalho />} />
     </Routes>
   );
 }
