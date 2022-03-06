f51ae7001d85b5eb9b9331c9ab46b65c838cbcb3
Luiz Bonfim <luiz.bonfim.neto@ccc.ufcg.edu.br>
2022-03-06 10:28:58 -0300

    creating footer component and refactoring css

---
+++ src/components/DefaultFooter/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { GithubOutlined, HeartFilled } from '@ant-design/icons';
+
+import './style.css';
+
+export default function DefaultFooter() {
+  return (
+    <div className="footer-container">
+      <footer className="footer-component">
+        <span>
+          WebApp criado em Engenharia de Software
+          <HeartFilled color="#F42C22" />
+        </span>
+        <span>
+          Conheça nosso
+          <a href="https://github.com/Health-Track/">
+            <GithubOutlined />
+          </a>
+        </span>
+      </footer>
+    </div>
+  );
+}
--- src/pages/ForgotPassword/index.js
+++ src/pages/ForgotPassword/index.js
@@ -2,35 +2,38 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Logo from '../../assets/logo.png';
+import DefaultFooter from '../../components/DefaultFooter';
+
+import './style.css';
 
 export default function ForgotPassword() {
   return (
-    <>
-      <div className="username-header" id="username-header" />
-      <Link to="/">
-        <img className="health-logo" src={Logo} alt="" />
-      </Link>
-      <div className="form-comum">
-        <h1>Login</h1>
-        <form>
-          <p>
-            <input type="text" id="email" value="" placeholder="E-mail" />
-          </p>
-          <p className="submit">
-            <input
-              type="submit"
-              id="commit"
-              onClick="validateEmail()"
-              className="submitBtn"
-              value="Recuperar"
-            />
-          </p>
-        </form>
+    <div className="forgot-password-container">
+      <div className="logo-container">
+        <Link to="/">
+          <img className="health-logo" src={Logo} alt="" />
+        </Link>
       </div>
-
-      <div id="footer" style={{ 'font-size': '10px' }}>
-        <p>ENGENHARIA DE SOFTWARE</p>
+      <div className="form-container">
+        <div className="form-comum">
+          <h1>Recupere sua senha</h1>
+          <form>
+            <p>
+              <input type="text" id="email" value="" placeholder="E-mail" />
+            </p>
+            <p className="submit">
+              <input
+                type="submit"
+                id="commit"
+                onClick="validateEmail()"
+                className="submitBtn"
+                value="Recuperar"
+              />
+            </p>
+          </form>
+        </div>
       </div>
-    </>
+      <DefaultFooter />
+    </div>
   );
 }
