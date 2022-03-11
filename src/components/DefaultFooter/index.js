1e0efd74b88509868da5922837a9a35781a66e3a
Luiz Bonfim <luiz.bonfim.neto@ccc.ufcg.edu.br>
2022-03-10 22:26:07 -0300

    making footer responsive

--- src/components/DefaultFooter/index.js
+++ src/components/DefaultFooter/index.js
@@ -7,11 +7,11 @@ export default function DefaultFooter() {
   return (
     <div className="footer-container">
       <footer className="footer-component">
-        <span>
+        <span className="es-span">
           WebApp criado em Engenharia de Software
           <HeartFilled color="#F42C22" />
         </span>
-        <span>
+        <span className="github-span">
           Conheça nosso
           <a href="https://github.com/Health-Track/">
             <GithubOutlined />
