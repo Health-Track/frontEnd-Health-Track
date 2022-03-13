c3f2a3df4b715eaf142eb33d134d4f754b0722c0
Itallo-Diniz <jose.diniz@ccc.ufcg.edu.br>
2022-03-12 23:15:03 -0300

    adcionado comportamento de click para abrir menu

--- src/components/DeafultHeader/index.js
+++ src/components/DeafultHeader/index.js
@@ -1,28 +1,33 @@
 import React from 'react';
 import './style.css';
-/* import { Button } from 'antd';
-import { MenuOutlined } from '@ant-design/icons'; */
 import Logo from '../../assets/logo.png';
 
-export default function DefaultHeader(props) {
-  return (
-    <div className="header">
-      <span className="botao-menu">
-        {/* <Button
-          icon={<MenuOutlined style={{ color: 'black' }} />}
-          type="link"
-          size="large"
-        /> */}
-        <div className="hamburguer" />
-        <div className="hamburguer" />
-        <div className="hamburguer" />
-      </span>
-      <span className="logo-header-span">
-        <img className="logo-header" src={Logo} alt="" />
-      </span>
-      <span className="user-info">
-        <h2>{props.username}</h2>
-      </span>
-    </div>
-  );
+export default class DefaultHeader extends React.Component {
+  render() {
+    return (
+      <div className="header-container">
+        <div className="header">
+          <span
+            className="botao-menu"
+            onClick={this.props.toggleMenu}
+            role="button"
+            tabIndex={0}
+            onKeyDown={() => {
+              console.log('lint chato');
+            }}
+          >
+            <div className="hamburguer" />
+            <div className="hamburguer" />
+            <div className="hamburguer" />
+          </span>
+          <span className="logo-header-span">
+            <img className="logo-header" src={Logo} alt="" />
+          </span>
+          <span className="user-info">
+            <h2>{this.props.username}</h2>
+          </span>
+        </div>
+      </div>
+    );
+  }
 }
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,14 +1,41 @@
 import React from 'react';
+import { Drawer } from 'antd';
+import 'antd/dist/antd.css';
 import DefaultFooter from '../../components/DefaultFooter';
 import DefaultHeader from '../../components/DeafultHeader';
 
-function HomePage() {
-  return (
-    <div className="homepage-container">
-      <DefaultHeader username="Itallo" />
-      <DefaultFooter />
-    </div>
-  );
+class HomePage extends React.Component {
+  constructor() {
+    super();
+    this.state = { showMenu: false };
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  toggleMenu() {
+    this.setState(prevState => ({
+      showMenu: !prevState.showMenu
+    }));
+    console.log(this.state.showMenu);
+  }
+
+  render() {
+    return (
+      <div className="homepage-container">
+        <DefaultHeader username="Itallo" toggleMenu={this.toggleMenu} />
+        <DefaultFooter />
+        <Drawer
+          title="Menu"
+          placement="left"
+          onClose={this.toggleMenu}
+          visible={this.state.showMenu}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Drawer>
+      </div>
+    );
+  }
 }
 
 export default HomePage;
