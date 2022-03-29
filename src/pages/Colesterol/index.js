5a243513c0548f349802e5d5cef476482b288bc5
Luiz Bonfim <luiz.bonfim.neto@ccc.ufcg.edu.br>
2022-03-28 21:17:00 -0300

    creating colesterol exam posting to the backend

--- src/pages/Colesterol/index.js
+++ src/pages/Colesterol/index.js
@@ -1,145 +1,125 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Button, Card, Table } from 'antd';
-import 'antd/dist/antd.css';
+
 import DefaultFooter from '../../components/DefaultFooter';
 import DefaultHeader from '../../components/DeafultHeader';
 import DefaultMenu from '../../components/DefaultMenu';
 import ColesterolForm from './form';
+import api from '../../services/apiClient';
+
 import './style.css';
 
-class Colesterol extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      showMenu: false,
-      showNovo: false,
-      registros: [
-        {
-          descricao:
-            'Descricao descricao descricao descricao descricaodescricao descricaodescricao descricaodescricao descricaodescricao descricaodescricao descricao descricao descricao descricao descricaodescricao descricaodescricao descricaodescricao descricao',
-          data: '2022-03-21T10:15:30',
-          colesterolTotal: '10',
-          colesterolHDL: '20',
-          colesterolNaoHDL: '30',
-          colesterolLDL: '40',
-          relacaoTotalHDL: '21.3'
-        },
-        {
-          descricao: 'Descricao',
-          data: '2022-03-21T10:15:30',
-          colesterolTotal: '10',
-          colesterolHDL: '20',
-          colesterolNaoHDL: '30',
-          colesterolLDL: '40',
-          relacaoTotalHDL: '21.3'
-        }
-      ]
-    };
-    this.toggleMenu = this.toggleMenu.bind(this);
-    this.toggleNovo = this.toggleNovo.bind(this);
+export default function Colesterol() {
+  const [showMenu, setShowMenu] = useState(false);
+  const [showNovoRegistroMenu, setShowNovoRegistroMenu] = useState(false);
+  const [registros, setRegistros] = useState([]);
+
+  const toggleNovoRegistroMenu = () => {
+    setShowNovoRegistroMenu(!showNovoRegistroMenu);
   }
 
-  toggleNovo() {
-    this.setState(prevState => ({
-      showNovo: !prevState.showNovo
-    }));
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
   }
 
-  toggleMenu() {
-    this.setState(prevState => ({
-      showMenu: !prevState.showMenu
-    }));
-  }
+  const columns = [
+    {
+      title: 'Data',
+      dataIndex: 'data',
+      key: 'data',
+      width: 150,
+      fixed: 'left'
+    },
+    {
+      title: 'Descrição',
+      fixed: 'left',
+      width: 300,
+      dataIndex: 'descricao',
+      key: 'descricao'
+    },
+    {
+      title: 'Colesterol Total',
+      width: 150,
+      dataIndex: 'colesterolTotal',
+      key: 'colesterolTotal'
+    },
+    {
+      title: 'Colesterol HDL',
+      width: 150,
+      dataIndex: 'colesterolHDL',
+      key: 'colesterolHDL'
+    },
+    {
+      title: 'Colesterol Nao HDL',
+      width: 150,
+      dataIndex: 'colesterolNaoHDL',
+      key: 'colesterolNaoHDL'
+    },
+    {
+      title: 'Colesterol LDL',
+      width: 150,
+      dataIndex: 'colesterolLDL',
+      key: 'colesterolLDL'
+    },
+    {
+      title: 'Relacao Total HDL',
+      dataIndex: 'relacaoTotalHDL',
+      width: 150,
+      key: 'relacaoTotalHDL'
+    }
+  ];
 
-  keyedList() {
-    return this.state.registros.map((item, idx) => {
-      item.key = idx;
-      return item;
-    });
-  }
+  useEffect(() => {
+    loadColesterolExams();
+  }, [registros]);
 
-  render() {
-    const columns = [
-      {
-        title: 'Data',
-        dataIndex: 'data',
-        key: 'data',
-        width: 150,
-        fixed: 'left'
-      },
-      {
-        title: 'Descrição',
-        fixed: 'left',
-        width: 300,
-        dataIndex: 'descricao',
-        key: 'descricao'
-      },
-      {
-        title: 'Colesterol Total',
-        width: 150,
-        dataIndex: 'colesterolTotal',
-        key: 'colesterolTotal'
-      },
-      {
-        title: 'Colesterol HDL',
-        width: 150,
-        dataIndex: 'colesterolHDL',
-        key: 'colesterolHDL'
-      },
-      {
-        title: 'Colesterol Nao HDL',
-        width: 150,
-        dataIndex: 'colesterolNaoHDL',
-        key: 'colesterolNaoHDL'
-      },
-      {
-        title: 'Colesterol LDL',
-        width: 150,
-        dataIndex: 'colesterolLDL',
-        key: 'colesterolLDL'
-      },
-      {
-        title: 'Relacao Total HDL',
-        dataIndex: 'relacaoTotalHDL',
-        width: 150,
-        key: 'relacaoTotalHDL'
-      }
-    ];
-    return (
-      <div className="Colesterol-container">
-        <DefaultHeader username="Username" toggleMenu={this.toggleMenu} />
-        <div>
-          <Card
-            title="Colesterol"
-            extra={
-              <Button type="primary" onClick={() => this.toggleNovo()}>
-                Novo
-              </Button>
-            }
-          >
-            <Table
-              dataSource={this.keyedList()}
+  const loadColesterolExams = useCallback(async () => {
+    await api.get('/exame/colesterol/listar')
+             .then(resp => {
+                setRegistros([...resp.data])
+             })
+             .catch(err => {
+                console.log(err);
+             })
+  })
+
+  return (
+    <div className="Colesterol-container">
+      <DefaultHeader username="Username" toggleMenu={toggleMenu} />
+      <div>
+        <Card
+          title="Colesterol"
+          extra={
+            <Button type="primary" onClick={toggleNovoRegistroMenu}>
+              Novo
+            </Button>
+          }
+        >
+          {registros.length > 0 &&
+          <Table
+              dataSource={registros.map((item, idx) => {
+                item.key = idx;
+                return item;
+              })}
               columns={columns}
               scroll={{ x: 1500, y: 350 }}
               pagination={false}
-            />
-          </Card>
-        </div>
-        <DefaultFooter />
-
-        <ColesterolForm
-          registros={this.state.registros}
-          showNovo={this.state.showNovo}
-          toogleNovo={this.toggleNovo}
-        />
-
-        <DefaultMenu
-          showMenu={this.state.showMenu}
-          toggleMenu={this.toggleMenu}
-        />
+          />}
+        </Card>
       </div>
-    );
-  }
-}
+      <DefaultFooter />
 
-export default Colesterol;
+      <ColesterolForm
+        registros={registros}
+        showNovo={showNovoRegistroMenu}
+        toogleNovo={toggleNovoRegistroMenu}
+      />
+
+      <DefaultMenu
+        showMenu={showMenu}
+        toggleMenu={toggleMenu}
+      />
+    </div>
+  );
+
+}
